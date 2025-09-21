0e4720f7de92c33cd8e63d4a21794e4b8faa2746
Long-Giang Vu <giangvl.cs@gmail.com>
2025-09-21 14:35:53 -0700

    WIP: sync job name and wandb run name

--- cvrunner/cli.py
+++ cvrunner/cli.py
@@ -69,7 +69,8 @@ def run_local(args: Namespace) -> None:
 
     runner_cls = exp.runner_cls()
     wandb_project = exp.wandb_project
-    wandb_runname = exp.wandb_runname
+    job_name = os.environ.get("JOB_NAME")
+    wandb_runname = job_name if job_name else exp.wandb_runname  # fallback for local runs
 
     if wandb_project is not None:
         logger.init_wandb(project=wandb_project, run_name=wandb_runname, config=vars(exp))
