85ea1aa6bcef4c69916187675f6d12cbfc52d224
Long Giang Vu <giangvl.cs@gmail.com>
2025-12-15 21:33:33 -0800

    feat: make it standalone

--- cvrunner/cli.py
+++ cvrunner/cli.py
@@ -69,6 +69,7 @@ def run_local(args: Namespace) -> None:
     # We use 'torchrun' to launch the DistributedTrainRunner script
     cmd = [
         "torchrun",
+        "--standalone", # Single-node
         "--nproc_per_node", str(num_gpus if num_gpus > 0 else 1),
         "--rdzv_backend", "c10d",
         "--rdzv_endpoint", "localhost:0", # Let OS pick a random free port
